fc41b41c679379c4b258e7b6faa86b4d476f7c91
Misha <mikael_2000@mail.ru>
2018-05-22 15:37:49 +0400

    added changePopularity api

--- functionality/changePopularity.js
+++ functionality/changePopularity.js
@@ -1,20 +1,18 @@
-// let {Goods} = require('../models/goods')
+let {Goods} = require('../models/goods')
 
-// let changePopularity = (req, res) => {
+let changePopularity = (req, res) => {
 
-//     let id = req.params.id
+    let id = req.params.id
 
-//     //     Goods.findOneAndUpdate({_id: id}, {$set: {popularity: +1 }}, {new: true}).then((goods) => {
-//     //         res.send(goods)
-//     //     })
-//         Goods.find({_id: id}).then((goods) => {
-//             let popularity = goods.popularity
-//             console.log(popularity)
-//                 Goods.findOneAndUpdate({goods}, {$set: {popularity: Number(popularity) +1}}, {new: true}).then((goods) => {
-//                     res.send(goods)
-//                 })
-//         })
+    //     Goods.findOneAndUpdate({_id: id}, {$set: {popularity: +1 }}, {new: true}).then((goods) => {
+    //         res.send(goods)
+    //     })
+        Goods.find({_id: id}).then((goods) => {
+            let popularity = goods[0].popularity;
+                Goods.findOneAndUpdate({_id: id}, {$set: {popularity: popularity + 1}}, {new: true}).then((goods) => {
+                    res.send(goods)
+                })
+        })
+}
 
-// }
-
-// module.exports = {changePopularity}
+module.exports = {changePopularity}
