d8977b766d52fdb0096b0d9b538d0a04d3236b13
Misha <mikael_2000@mail.ru>
2018-05-29 15:04:06 +0400

    fixed some bugs

--- authenticate/authenticate.js
+++ authenticate/authenticate.js
@@ -1,18 +1,18 @@
 const {Admin} = require('../models/admin');
-
+const {logger} = require('../logger/logger')
 
 let authenticate = (req, res, next) => {
     let token = req.header('x-auth');
     Admin.findByToken(token).then((admin) => {
-        if(!user){
+        if(!admin){
             return res.send('You are not Admin!')
         }
             req.admin = admin;
             req.token = token;   
             next();       
     },(e) => {
-        res.status(401).send(e);
-        })
+        res.status(401).logger.error(e);
+        }).catch((e) => {logger.debug(e)})
 } 
 
 module.exports = {authenticate};
