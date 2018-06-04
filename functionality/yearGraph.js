7e119a0ab0f98d6f559f7f76147a0d4a0ff2c1d0
Misha <mikael_2000@mail.ru>
2018-06-04 17:35:45 +0400

    added yearGraph functionality

---
+++ functionality/yearGraph.js
@@ -0,0 +1,28 @@
+const {Order} = require('../models/order')
+const {logger} = require('../logger/logger')
+const moment = require('moment')
+
+let yearGraph = (req, res) => {
+    let now = moment();
+    let data = [];
+    let income;
+    let soldPrice;
+    let createDate;
+    Order.find({status: 2}).then((orders) => {
+        orders.forEach((order) => {
+            soldPrice = order.items[0].wholesale || order.items[0].retail;
+            income = (soldPrice - order.items[0].price)* orders.length
+            createDate = moment(order.createdAt);
+            if(now.year() == createDate.year()){
+                if(data[createDate.date() - 1]) {
+                    data[createDate.date() - 1] += income
+                } else {
+                    data[createDate.date() - 1] = income
+                }
+            }
+        })
+        res.send(data);
+    }).catch((e) => logger.debug(e))
+}
+
+module.exports = {yearGraph}
